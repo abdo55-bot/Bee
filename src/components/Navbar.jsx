7646af244bb18e33014b304cd193b0f0e7097863
abdelrahamn <bodaahmed2500@gmail.com>
2024-03-14 16:15:47 +0200

    fix navbar container

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
         <Disclosure as="nav" className={dark ? 'bg-black text-white' : 'bg-white text-black'}  >
             {({ open }) => (
                 <>
-                    <div className="mx-auto max-w-7xl px-2 py-2 sm:px-6 lg:px-8">
+                    <div className="mx-auto max-w-7xl px-2 py-2 sm:px-6 lg:px-8 container">
                         <div className="relative flex flex-row-reverse h-16 items-center justify-between">
                             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                                 {/* Mobile menu button*/}
