0a1cdeeaf9763f2ff4cc613829679334e6729687
abdelrahamn <bodaahmed2500@gmail.com>
2024-03-14 16:20:00 +0200

    remove CTA2 because its like CTA and rerender CTA

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import Hero from '../components/Hero'
 import CTA from '../components/CTA'
 import EventList from '../pages/Events/EventList'
-import CTA2 from '../components/CTA2'
 import CourseList from '../pages/courses/CourseList'
 
 const Home = () => {
@@ -10,7 +9,7 @@ const Home = () => {
       <Hero />
       <CTA  />
       <EventList />
-      <CTA2 />
+      <CTA/>
       <CourseList />
     </>
   )
