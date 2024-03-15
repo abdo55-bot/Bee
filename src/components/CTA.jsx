53e9f43d3aed5c8ebb51757847822c7ba28b5140
abdelrahamn <bodaahmed2500@gmail.com>
2024-03-15 23:00:44 +0200

    add bg to icon

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -36,8 +36,8 @@ const CTA = () => {
                                 <Link to="/eventDetails/1" className='rounded-full flex items-center text-white bg-primary font-medium  px-12 shadow-main '>
                                     اذهب الى الدورة
                                 </Link>
-                                <button className={`rounded-full w-14 h-14 bg-white flex justify-center items-center  font-medium `} >
-                                     <BsBookmark  className={ `text-2xl text-black ` }/>
+                                <button className={`rounded-full w-14 h-14 bg-white flex justify-center items-center  font-medium hover:bg-primary duration-150`} >
+                                     <BsBookmark  className={ `text-2xl text-black  ` }/>
                                  </button>
                             </div>
                         </div>
